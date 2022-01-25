7fa07469da8724ab6aaee2a7bcbc2bbaccaab37a
Hwang <dudhkd98@naver.com>
2022-01-25 22:47:12 +0900

    [FEAT]: Identifiers setting

---
+++ ITZZA/ITZZA/Global/Struct/Identifiers.swift
@@ -0,0 +1,26 @@
+//
+//  Identifiers.swift
+//  ITZZA
+//
+//  Created by 황윤경 on 2022/01/25.
+//
+
+import Foundation
+struct Identifiers {
+    //MARK: - Storyboard
+    static let tabBarSB = "TabBar"
+    static let homeSB = "Home"
+    static let communitySB = "Community"
+    static let mypageSB = "Mypage"
+    
+    //MARK: - Controller
+    static let itzzaTBC = "ITZZATBC"
+    static let homeVC = "HomeVC"
+    static let communityVC = "CommunityVC"
+    static let mypageVC = "MypageVC"
+    
+    //MARK: - TVC
+    
+    //MARK: - CVC
+    
+}
