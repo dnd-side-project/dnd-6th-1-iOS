5e61a13163876d4c815b5ab4c070e439109a87dd
Hwang <dudhkd98@naver.com>
2022-02-02 01:48:32 +0900

    [FEAT]: add post contents and design components
    - issue: #5

--- ITZZA/ITZZA/Screen/Community/Cell/TVC/ArticleTVC.swift
+++ ITZZA/ITZZA/Screen/Community/Cell/TVC/ArticleTVC.swift
@@ -8,10 +8,17 @@
 import UIKit
 
 class ArticleTVC: UITableViewCell {
-
+    @IBOutlet weak var headerView: ProfileHeaderView!
+    @IBOutlet weak var footerView: PostButtonsView!
+    @IBOutlet weak var contents: UITextView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        contents.isScrollEnabled = false
+        contents.isEditable = false
+        contents.isSelectable = false
+        contents.backgroundColor = .clear
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
