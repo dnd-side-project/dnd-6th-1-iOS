b185f51324fd717f4c3b0ba11707bbdcd6ecac59
Hwang <dudhkd98@naver.com>
2022-02-02 03:01:58 +0900

    [CHORE]: DataSource 구조 변경 1
    - issue: #5

--- ITZZA/ITZZA/Screen/Community/View/Controller/CategoryVC.swift
+++ ITZZA/ITZZA/Screen/Community/View/Controller/CategoryVC.swift
@@ -14,32 +14,43 @@ class CategoryVC: UIViewController {
     @IBOutlet weak var postListTV: UITableView!
     
     let bag = DisposeBag()
+    let dummyData = ["사용자닉", "용자닉네", "자닉네임", "갸악"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        postListTV.dataSource = self
-        postListTV.backgroundColor = .systemGray6
+        bindTV()
+        setPostTV()
     }
 }
 
 //MARK: - Custom Methods
 extension CategoryVC {
-    func bindTV() {
-
-    }
-}
-
-extension CategoryVC: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        30
+    func setPostTV() {
+        postListTV.backgroundColor = .systemGray6
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: Identifiers.postTVC, for: indexPath) as! PostTVC
-        
-        cell.backgroundColor = .clear
-        
-        return cell
+    func bindTV() {
+        let configureCell: (TableViewSectionedDataSource<SectionModel<String, String>>, UITableView,IndexPath, String) -> UITableViewCell = { (datasource, tableView, indexPath,  element) in
+
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: Identifiers.postTVC, for: indexPath) as? PostTVC else { return UITableViewCell() }
+            cell.backgroundColor = .clear
+            cell.headerView.userName.text = element
+            return cell
+        }
+
+        let datasource = RxTableViewSectionedReloadDataSource<SectionModel<String, String>>.init(configureCell: configureCell)
+
+        datasource.titleForHeaderInSection = { datasource, index in
+            return datasource.sectionModels[index].model
+        }
+
+        let sections = [
+            SectionModel<String, String>(model: "", items: dummyData)
+        ]
+
+        Observable.just(sections)
+            .bind(to: postListTV.rx.items(dataSource: datasource))
+            .disposed(by: bag)
     }
 }
