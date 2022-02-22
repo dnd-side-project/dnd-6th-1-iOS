73677e0052df82d32c173c390327c4edcbe02f89
injeChoi <2015jasonc@gmail.com>
2022-02-22 12:55:14 +0900

    [FIX]: wrong branch

--- ITZZA/ITZZA/Screen/Home/View/Shared/XibController/EmptyDiaryView.swift
+++ ITZZA/ITZZA/Screen/Home/View/Shared/XibController/EmptyDiaryView.swift
@@ -11,17 +11,25 @@ import RxCocoa
 
 class EmptyDiaryView: UIView {
     
+    @IBOutlet weak var contentView: PostWriteView!
+    @IBOutlet weak var imageScrollView: ImageScrollView!
+    
     var disposeBag = DisposeBag()
+    var dummyArray = [UIImage(named: "Emoji_Sad")!, UIImage(named: "Emoji_Sad")!, UIImage(named: "Emoji_Sad")!]
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         setContentView()
+        configureContentView()
+        configureImageScrollView()
         setInitialUIValue()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         setContentView()
+        configureContentView()
+        configureImageScrollView()
         setInitialUIValue()
     }
     
@@ -35,6 +43,16 @@ extension EmptyDiaryView {
     func setInitialUIValue() {
         self.layer.masksToBounds = true
         self.layer.cornerRadius = 3
+    }
+    
+    func configureContentView() {
+        contentView.setTitlePlaceholder("asd")
+        contentView.setContentsPlaceholder("sldkjfgbsldfgul")
+    }
+    
+    func configureImageScrollView() {
+        imageScrollView.image = dummyArray
+        imageScrollView.configurePost()
     }
 }
 
