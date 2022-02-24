920469d73830795efe470c6980bbd3dd7edcaaea
Hwang <dudhkd98@naver.com>
2022-02-24 15:06:25 +0900

    [FIX]: conflict miss 수정 - issue: #52

--- ITZZA/ITZZA/Global/Network/APISession.swift
+++ ITZZA/ITZZA/Global/Network/APISession.swift
@@ -45,6 +45,35 @@ struct APISession: APIService {
         }
     }
     
+    func getRequest<T>(with urlResource: urlResource<T>) -> Observable<Result<T, APIError>> where T : Decodable {
+        
+        return Observable<Result<T, APIError>>.create { observer in
+            
+            guard let token: String = KeychainWrapper.standard[.myToken] else { return Disposables.create { } }
+            
+            let header: HTTPHeaders = [
+                "Authorization": "Bearer \(token)"
+            ]
+            
+            let task = AF.request(urlResource.url, headers: header)
+                .validate(statusCode: 200...399)
+                .responseDecodable(of: T.self) { response in
+                    switch response.result {
+                    case .failure(let error):
+                        print("Unknown HTTP Response Error!!!: \(error.localizedDescription)")
+                        observer.onNext(.failure(.unknown))
+                        
+                    case .success(let decodedData):
+                        observer.onNext(.success(decodedData))
+                    }
+                }
+            
+            return Disposables.create {
+                task.cancel()
+            }
+        }
+    }
+    
     func postRequestWithImages<T: Decodable>(with urlResource: urlResource<T>, param: Parameters, images: [UIImage]) -> Observable<Result<T, APIError>> {
         Observable<Result<T, APIError>>.create { observer in
             guard let token: String = KeychainWrapper.standard[.myToken] else { return Disposables.create{} }
