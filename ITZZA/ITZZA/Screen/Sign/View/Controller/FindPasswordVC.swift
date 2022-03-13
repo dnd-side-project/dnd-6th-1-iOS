f243e99badcb518a5e284fb7f7a23e8f4a6c56b8
injeChoi <2015jasonc@gmail.com>
2022-03-13 16:10:46 +0900

    [VIEW]: 디렉토리 구성 - issue: #93

---
+++ ITZZA/ITZZA/Screen/Sign/View/Controller/FindPasswordVC.swift
@@ -0,0 +1,18 @@
+//
+//  FindPasswordVC.swift
+//  ITZZA
+//
+//  Created by InJe Choi on 2022/03/13.
+//
+
+import UIKit
+
+class FindPasswordVC: UIViewController {
+    
+    @IBOutlet weak var viewTitleLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
